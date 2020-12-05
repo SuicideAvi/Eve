97f78a416e2d8535d3373edf79a73ffeb5a0fe24
SuicideAvi <kenvillapando09@gmail.com>
2020-12-05 11:07:06 +0100

    Use process env instead of config json

---
+++ welcome.js
@@ -0,0 +1,11 @@
+module.exports = client => {
+    const channelId = '729655970099363931' // welcome channel
+    client.on('guildMemberAdd', (member) => {
+        console.log(member)
+
+        const message = `please welcome <@${member.id}> to the server`
+
+        const channel = member.guild.channels.cache.get(channelId)
+        channel.send(message)
+    })
+}
