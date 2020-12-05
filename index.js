da3dc29ee9ce0dd8836cf9cd583656d284108c07
SuicideAvi <kenvillapando09@gmail.com>
2020-12-05 12:58:04 +0100

    fix

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ client.on('ready', () => {
         message.channel.send(`   
     These are my supported commands:
 
-    **-help**                      = _Displays the help menu_
-    **-ping**                          = _Ping the bot_
-    **-life**                            = _tells you about your life_
+    **-help/h**                     = _Displays the help menu_
+    **-ping**                           = _Ping the bot_
+    **-life**                           = _tells you about your life_
     **-cc/clearchannel**    = _purge channel_
-    **-kick/yeet**               = _kick members_
-    **-ban**                          = _ban members_
-    **-serverinfo/si**         = _Server info_
+    **-kick/yeet**              = _kick members_
+    **-ban**                            = _ban members_
+    **-serverinfo/si**          = _Server info_
     **-poll**                           = _do a poll on the previous message_
      `)
     })
